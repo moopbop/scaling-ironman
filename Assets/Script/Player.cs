ea0e05aaea51f03baa7e167377e42b9d181ed830
Patrick Bartlett <moopbop@gmail.com>
2015-05-11 14:16:25 -0400

    Rigidbody movement

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,77 +8,82 @@ public class Player : MonoBehaviour {
 	public float bulletSpeed;	// Allows for speed powerups
 
 	private Vector3 updatePos;	// Temp Vector3 
+	private Rigidbody rb;
+
+	void Start() {
+		rb = this.GetComponent<Rigidbody> ();
+	}
 
 	void Update() {
 
-		float increment = speed * Time.deltaTime;
-
-		#region movement
-		if (Input.GetKey (KeyCode.W)) {
-//			updatePos = transform.position;
-//			updatePos.y += speed * increment;
-//			transform.position = Vector3.MoveTowards(transform.position, updatePos, increment);
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y + increment, transform.position.z), increment);
-		}
-
-		if (Input.GetKey (KeyCode.A)) {
-//			updatePos = transform.position;
-//			updatePos.x -= speed * increment;
-//			transform.position = Vector3.MoveTowards(transform.position, updatePos, increment);
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x - increment, transform.position.y, transform.position.z), increment);
-		}
-
-		if (Input.GetKey (KeyCode.S)) {
-//			updatePos = transform.position;
-//			updatePos.y -= speed * increment;
-//			transform.position = Vector3.MoveTowards(transform.position, updatePos, increment);
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y - increment, transform.position.z), increment);
-		}
-
-		if (Input.GetKey (KeyCode.D)) {
-//			updatePos = transform.position;
-//			updatePos.x += speed * increment;
-//			transform.position = Vector3.MoveTowards(transform.position, updatePos, increment);
-			transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x + increment, transform.position.y, transform.position.z), increment);
-		}
-		#endregion
-
 		#region shooting
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			GameObject o = Instantiate (bullet, this.transform.position, Quaternion.identity) as GameObject;
+			GameObject o = Instantiate (bullet, this.rb.transform.position, Quaternion.identity) as GameObject;
 			o.GetComponent<Projectile>().direction = new Vector3(-1, 0, 0);
 			o.GetComponent<Projectile>().speed = this.bulletSpeed;
 		}
 		else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			GameObject o = Instantiate (bullet, this.transform.position, Quaternion.identity) as GameObject;
+			GameObject o = Instantiate (bullet, this.rb.transform.position, Quaternion.identity) as GameObject;
 			o.GetComponent<Projectile>().direction = new Vector3(1, 0, 0);
 			o.GetComponent<Projectile>().speed = this.bulletSpeed;
 		}
 		else if (Input.GetKeyDown (KeyCode.UpArrow)) {
-			GameObject o = Instantiate (bullet, this.transform.position, Quaternion.identity) as GameObject;
+			GameObject o = Instantiate (bullet, this.rb.transform.position, Quaternion.identity) as GameObject;
 			o.GetComponent<Projectile>().direction = new Vector3(0, 1, 0);
 			o.GetComponent<Projectile>().speed = this.bulletSpeed;
 		}
 		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			GameObject o = Instantiate (bullet, this.transform.position, Quaternion.identity) as GameObject;
+			GameObject o = Instantiate (bullet, this.rb.transform.position, Quaternion.identity) as GameObject;
 			o.GetComponent<Projectile>().direction = new Vector3(0, -1, 0);
 			o.GetComponent<Projectile>().speed = this.bulletSpeed;
 		}
 		#endregion
 
 		#region position clamping
-		if (transform.position.x < -8.5) {
+		if (rb.transform.position.x < -8.5) {
 			transform.position = new Vector2(-8.5f, transform.position.y);
 		}
-		if (transform.position.x > 8.5) {
+		if (rb.transform.position.x > 8.5) {
 			transform.position = new Vector2(8.5f, transform.position.y);
 		}
-		if (transform.position.y < -4.5) {
+		if (rb.transform.position.y < -4.5) {
 			transform.position = new Vector2(transform.position.x, -4.5f);
 		}
-		if (transform.position.y > 4.5) {
+		if (rb.transform.position.y > 4.5) {
 			transform.position = new Vector2(transform.position.x, 4.5f);
 		}
 		#endregion
+	}
+
+	void FixedUpdate() {
+
+		#region movement
+		if (Input.GetKey (KeyCode.W)) {
+			rb.AddForce (new Vector3 (0, speed, 0));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			rb.AddForce (new Vector3 (-speed, 0, 0));
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			rb.AddForce (new Vector3 (0, -speed, 0));
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rb.AddForce (new Vector3 (speed, 0, 0));
+		}
+		#endregion
+	}
+
+	void OnTriggerEnter(Collider c) {
+		string colTag = c.gameObject.tag;
+		Debug.Log ("I HIT THE THING");
+
+		if (colTag == "pwp_speed_up") {
+			this.bulletSpeed += 100;
+			Destroy (c.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision c) {
+		Debug.Log ("Collide?");
 	}
 }
